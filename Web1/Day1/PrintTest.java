3ed328c0f9271ae82dde7563dd989b0cab8b67dd
Taeho <65759092+bueeciel9@users.noreply.github.com>
2020-05-24 21:09:23 -0500

    Day_1

---
+++ Web1/Day1/PrintTest.java
@@ -0,0 +1,23 @@
+package day01;
+
+public class PrintTest {
+	public static void main(String[] args) {
+		
+		//ctrl + / : 주석, 해제
+//		주석 : 해석하고 싶지 않은 줄에 사용하는 기법
+		//1. 소스코드에 설명글을 달 때
+		//2. 기존에 작성되어 있던 소스코드가 당장 필요없을 때 사용한다.
+		
+		  /*
+		   * 범위 주석
+		   * ctrl + shift + /	: 주석
+		   * ctrl + shift + \	: 해제
+		   */ 
+		
+		//인사하는 영역
+		//System.out.print("안녕하세요");
+		
+		//소개하는 영역
+		//System.out.print("한동석 강사입니다.");
+	}
+}
---
+++ Web1/Day1/PrintTest2.java
@@ -0,0 +1,26 @@
+package day01;
+
+public class PrintTest2 {
+	public static void main(String[] args) {
+		//syso
+		//ctrl + alt + 방향키(위, 아래) : 복사
+		//alt + 방향키(위, 아래) : 이동
+		
+		//제어문자
+		//반드시 따옴표 안에서 작성한다.
+		// \n	: 줄바꿈(개행문자)
+		// \t	: 위아래 간격 맞춰 띄기(Tab)
+		// \'	: ' 표현
+		// \"	: " 표현
+		// \\	: \ 표현(경로 작성시 많이 사용됨)
+//		System.out.print("안녕하세요\n");
+		System.out.println("안녕하세요");
+		System.out.print("\"한동석\" 강사입니다.");
+	}
+}
+
+
+
+
+
+
